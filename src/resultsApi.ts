5f4e08fec4d7575caad46601acdf0d0acc1d886b
Robert Valek <rvalek@intersog.com>
2021-06-11 23:33:29 +0300

    results API

---
+++ src/resultsApi.ts
@@ -0,0 +1,32 @@
+export const publishResult = async ({ playerName, chapterPlayed, score, gameLength }) => {
+  const currentResults = (await getResults()).result.content;
+  const newResult = currentResults.concat([
+    { tag: "p", children: [`${playerName} | ${chapterPlayed} | ${score} из ${gameLength}`] },
+  ]);
+
+  // const base = [{ tag: "b", children: [`Имя Студента | Глава | Правильных Ответов`] }, { tag: "br" }];
+
+  const data = {
+    access_token: "5ca21c6a6b0d313c0b72032da5e3179cdf0bf5726aa2a20af5aa46b92853",
+    title: "Exam Results",
+    author_name: "Krishna Exam",
+    content: encodeURIComponent(JSON.stringify(newResult)),
+    return_content: true,
+  };
+
+  const dataAsParams = Object.entries(data).reduce((acc, [key, value]) => `${acc}${key}=${value}&`, "");
+
+  await fetch(`https://api.telegra.ph/editPage/Exam-Results-06-11`, {
+    method: "POST",
+    mode: "no-cors",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    body: dataAsParams,
+  });
+};
+
+const getResults = async () => {
+  const res = await fetch("https://api.telegra.ph/getPage/Exam-Results-06-11?return_content=true");
+  return await res.json();
+};
